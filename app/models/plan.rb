921c459170e2dfb9fcf376f7946030c222d55fba
Erik Hebisch <flegelleicht@gmail.com>
2018-10-18 13:43:18 +0200

    Added associations in the model classes

--- app/models/plan.rb
+++ app/models/plan.rb
@@ -1,2 +1,5 @@
 class Plan < ApplicationRecord
+  has_many :events
+  
+  validates :title, presence:true, length: { minimum: 1 }
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root 'visitors#index'
   
   resources :dockets
-  resources :plans
+  resources :plans do
+    resources :events
+  end
 end
