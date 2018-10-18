83fc19f2736a47087a0b5b459f465a175548497e
Erik Hebisch <flegelleicht@gmail.com>
2018-10-18 20:34:38 +0200

    Created and ran migration to associate plans with a user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_18_181657) do
+ActiveRecord::Schema.define(version: 2018_10_18_183244) do
 
   create_table "dockets", force: :cascade do |t|
     t.date "start"
@@ -36,6 +36,8 @@ ActiveRecord::Schema.define(version: 2018_10_18_181657) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_plans_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
