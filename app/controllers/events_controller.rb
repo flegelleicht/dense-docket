dde2784498f5388e8a2a8d0434ea275b492ef13f
Erik Hebisch <flegelleicht@gmail.com>
2018-10-18 14:00:46 +0200

    Added event creation to events controller

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,2 +1,13 @@
 class EventsController < ApplicationController
+  def create
+    @plan = Plan.find(params[:plan_id])
+    @event = @plan.events.create(event_params)
+    redirect_to plan_path(@plan)
+  end
+  
+  private
+  
+  def event_params
+    params.require(:event).permit(:title, :start, :end)
+  end
 end
