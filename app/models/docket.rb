b0ec0f9c1d0b105bace06d1b663cd1560f88a214
Erik Hebisch <flegelleicht@gmail.com>
2018-10-19 00:05:47 +0200

    Added showing events in a docket

--- app/models/docket.rb
+++ app/models/docket.rb
@@ -3,6 +3,7 @@ class Docket < ApplicationRecord
   
   # has_many :plans, through: :dockets_plans
   has_and_belongs_to_many :plans
+  has_many :events, through: :plans
   
   def weeks
     Enumerator.new do |e|
@@ -16,7 +17,6 @@ class Docket < ApplicationRecord
     end
   end
   
-
   def first_monday_around
     self.start - self.start.wday + 1
   end
