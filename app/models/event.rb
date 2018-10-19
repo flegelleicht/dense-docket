121076fdee8fcaa40ff1a3342b933e953e8c3453
Erik Hebisch <flegelleicht@gmail.com>
2018-10-19 12:04:59 +0200

    Added showing events and lead days with bars in the docket

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,8 +3,10 @@ class Event < ApplicationRecord
   validates :title, presence: true, length: { minimum: 1 }
   
   def is_on?(date)
-    almost= if self.lead_days 
+    almost = if self.lead_days && self.start
       then date >= (self.start - self.lead_days) && date < self.start
+    elsif self.lead_days && self.end
+      then date >= (self.end - self.lead_days) && date < self.end
     else 
       false 
     end
