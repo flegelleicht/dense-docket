9d760ad436a985926a24234005c7281f906f1cc0
Erik Hebisch <flegelleicht@gmail.com>
2018-10-19 11:30:13 +0200

    Added lead_days consideration to event

--- app/models/event.rb
+++ app/models/event.rb
@@ -3,6 +3,14 @@ class Event < ApplicationRecord
   validates :title, presence: true, length: { minimum: 1 }
   
   def is_on?(date)
-    date >= self.start && date <= self.end
+    almost= if self.lead_days 
+      then date >= (self.start - self.lead_days) && date < self.start
+    else 
+      false 
+    end
+    began = if self.start then date >= self.start else false end
+    over  = if self.end   then date >  self.end   else false end
+    
+    almost || (began && !over)
   end
 end
